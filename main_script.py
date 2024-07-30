0984c29a227f94fa4cf2616a515413b1b689c3c8
xmg8 <152449187+xmg8@users.noreply.github.com>
2024-07-30 19:39:20 +0800

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -7,6 +7,8 @@ import random
 import time
 import psutil
 import os
+import webbrowser
+from threading import Thread
 from requests.exceptions import ConnectionError, Timeout, SSLError
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from tkhtmlview import HTMLLabel
@@ -19,6 +21,8 @@ class App:
     def __init__(self, root):
         self.root = root
         self.root.title("XMG游戏团队")
+        
+        self.running = False  # 脚本运行状态
         
         self.show_welcome_message()  # 显示欢迎消息
         
@@ -45,8 +49,14 @@ class App:
         self.add_id_button = ttk.Button(root, text="添加ID", command=self.add_id)
         self.add_id_button.pack(pady=5)
         
-        self.run_button = ttk.Button(root, text="运行脚本", command=self.run_script)
-        self.run_button.pack(pady=5)
+        self.start_button = ttk.Button(root, text="开始领取", command=self.start_script)
+        self.start_button.pack(pady=5)
+        
+        self.stop_button = ttk.Button(root, text="停止领取", command=self.stop_script)
+        self.stop_button.pack(pady=5)
+        
+        self.automanage_button = ttk.Button(root, text="全自动托管", command=self.open_automanage)
+        self.automanage_button.pack(pady=5)
         
         self.text_area = ScrolledText(root, wrap=tk.WORD, width=100, height=20, font=('Helvetica', 12))
         self.text_area.pack(pady=10, padx=10)
@@ -63,10 +73,9 @@ class App:
                 file.write(content)
 
     def log(self, message):
-        if "成功" in message or "失败" in message:  # 仅记录成功或失败的消息
-            self.text_area.insert(tk.END, message + '\n')
-            self.text_area.see(tk.END)
-            self.root.update()
+        self.text_area.insert(tk.END, message + '\n')
+        self.text_area.see(tk.END)
+        self.root.update()
 
     def add_id(self):
         player_id = self.id_entry.get()
@@ -195,6 +204,19 @@ class App:
         except Exception as e:
             self.log(f"获取公告失败: {e}")
 
+    def start_script(self):
+        self.running = True
+        self.log("开始领取任务")
+        self.script_thread = Thread(target=self.run_script)
+        self.script_thread.start()
+
+    def stop_script(self):
+        self.running = False
+        self.log("停止领取任务")
+
+    def open_automanage(self):
+        webbrowser.open('http://www.xmg888.top')
+
     def run_script(self):
         ids_and_passwords = self.read_ids_and_passwords('ids.txt')
         unique_ids_and_passwords = list(set(ids_and_passwords))
@@ -203,19 +225,20 @@ class App:
         # 过滤掉已经成功签到的ID
         ids_to_run = [(id, pwd) for id, pwd in unique_ids_and_passwords if id not in successful_ids]
 
-        self.log(f"读取到的ID总数: {len(unique_ids_and_passwords)}")
-        self.log(f"成功的ID总数: {len(successful_ids)}")
-        self.log(f"失败的ID总数: {len(failed_ids)}")
-        self.log(f"需要执行任务的ID总数: {len(ids_to_run)}")
-        self.log(f"需要执行任务的ID列表: {ids_to_run}")
+        total_ids = len(ids_to_run)
+        self.log(f"需要执行任务的ID总数: {total_ids}")
 
         for idx, (player_id, password) in enumerate(ids_to_run):
+            if not self.running:
+                break
+            
             current_task_number = idx + 1
-            self.log(f"正在执行第 {current_task_number}/{len(ids_to_run)} 个任务: 玩家ID {player_id}")
+            self.log(f"正在执行第 {current_task_number}/{total_ids} 个任务: 玩家ID {player_id}")
             self.print_system_usage()  # 打印系统资源使用情况
 
             token = self.login(player_id, password)
             if not token:
+                self.log(f"玩家 {player_id} 登录失败，停止执行该ID任务")
                 with open('results.txt', 'a') as file:
                     file.write(f"{datetime.now()} 玩家 {player_id} 登录失败\n")
                 failed_ids.add(player_id)
@@ -223,6 +246,7 @@ class App:
 
             checkin_details = self.get_checkin_details(token, player_id)
             if not checkin_details:
+                self.log(f"玩家 {player_id} 获取每日签到详情失败，停止执行该ID任务")
                 with open('results.txt', 'a') as file:
                     file.write(f"{datetime.now()} 玩家 {player_id} 获取每日签到详情失败\n")
                 failed_ids.add(player_id)
@@ -234,21 +258,24 @@ class App:
                     if day_info['status'] == 1:  # 检查是否可以领取
                         no_task = False
                         checkin_day = int(day_info['name_language_code'].replace('第', '').replace('日', ''))
-                        for attempt in range(1, 6):
+                        for attempt in range(1, 4):
                             checkin_response = self.daily_checkin(token, player_id, checkin_day)
                             if checkin_response and checkin_response['code'] == 1:
+                                self.log(f"玩家 {player_id} 第{checkin_day}天签到成功")
                                 with open('results.txt', 'a') as file:
                                     file.write(f"{datetime.now()} 玩家 {player_id} 第{checkin_day}天签到成功\n")
                                 successful_ids.add(player_id)
                                 break
                             else:
-                                self.log(f"第{checkin_day}天签到失败，重试 {attempt}/5 次: {checkin_response}")
+                                self.log(f"玩家 {player_id} 第{checkin_day}天签到失败，重试 {attempt}/3 次: {checkin_response}")
                                 time.sleep(2)
                         else:
+                            self.log(f"玩家 {player_id} 第{checkin_day}天签到最终失败")
                             with open('results.txt', 'a') as file:
-                                file.write(f"{datetime.now()} 玩家 {player_id} 第{checkin_day}天签到最终失败: {checkin_response}\n")
+                                file.write(f"{datetime.now()} 玩家 {player_id} 第{checkin_day}天签到最终失败\n")
                             failed_ids.add(player_id)
             if no_task:
+                self.log(f"玩家 {player_id} 没有可领取的每日签到任务或任务已完成")
                 with open('results.txt', 'a') as file:
                     file.write(f"{datetime.now()} 玩家 {player_id} 没有可领取的每日签到任务或任务已完成\n")
                 successful_ids.add(player_id)
@@ -257,7 +284,7 @@ class App:
             time.sleep(random.randint(3, 6))
 
     def show_welcome_message(self):
-        welcome_message = "欢迎使用XMG游戏团队工具。\n请在下面的输入框中输入玩家ID并点击'Add ID'按钮添加，然后点击'Run Script'按钮运行脚本。"
+        welcome_message = "欢迎使用XMG游戏团队工具。\n请在下面的输入框中输入玩家ID并点击'添加ID'按钮添加，然后点击'开始领取'按钮运行脚本。"
         tkinter.messagebox.showinfo("欢迎", welcome_message)
 
 if __name__ == '__main__':
